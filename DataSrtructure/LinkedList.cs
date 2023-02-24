5b818e584ef57de39069a725ed1ad0add7273dde
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-24 15:23:11 +0530

    Modified to Create Linked List By Adding program

--- DataSrtructure/LinkedList.cs
+++ DataSrtructure/LinkedList.cs
@@ -27,5 +27,26 @@ namespace DataSrtructure
             }
             Console.WriteLine("{0} is inserted into linked list.",node.data);
         }
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            Console.WriteLine("{0} is added into linked list",newNode.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            Console.WriteLine("Linked list is: ");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
