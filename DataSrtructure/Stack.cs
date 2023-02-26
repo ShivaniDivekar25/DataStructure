2c6a53d3cd99019b7d70e6d9ad07574426f569d0
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-26 21:41:32 +0530

    Modified Create Peak And Pop Method program

--- DataSrtructure/Stack.cs
+++ DataSrtructure/Stack.cs
@@ -29,5 +29,33 @@ namespace DataStructure
                 temp = temp.next;
             }
         }
+        public void Peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("No element present in the stack");
+                return;
+            }
+            Console.WriteLine("\nTop most element " + this.top.data);
+        }
+        public void Pop()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is empty, deleting is not possible");
+                return;
+            }
+            Console.WriteLine("value popped is {0}",this.top.data);
+            this.top = this.top.next;
+        }
+        public void IsEmpty()
+        {
+            while(this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            Console.WriteLine("The stack is empty");
+        }
     }
 }
